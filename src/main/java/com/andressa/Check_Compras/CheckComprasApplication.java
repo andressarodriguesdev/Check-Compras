2c0ce4d5eb087e89795eefdb690764c63e9fc198
Andressa <andressa.rodrigues.2172@gmail.com>
2025-05-07 02:15:29 -0300

    Estrutura do projeto

---
+++ src/main/java/com/andressa/Check_Compras/CheckComprasApplication.java
@@ -0,0 +1,13 @@
+package com.andressa.Check_Compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckComprasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andressa/Check_Compras/CheckComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andressa.Check_Compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
